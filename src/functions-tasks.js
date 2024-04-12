baf8d0f52141fc2c25811b27f82816e87f44c8a5
asubas <znaxor74@mail.ru>
2024-04-12 22:17:42 +0300

    implement getIdGenerator

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -235,8 +235,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let index = startFrom;
+  return () => {
+    index += 1;
+    return index - 1;
+  };
 }
 
 module.exports = {
